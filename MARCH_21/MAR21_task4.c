f39c00722b34fe07c894e40130a67ef16decb94b
VardanKocharyan <vardanqocharyan5@gmail.com>
2025-04-15 23:28:01 +0400

    Update MAR21_task4.c

--- MARCH_21/MAR21_task4.c
+++ MARCH_21/MAR21_task4.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 
 int main(){
-
 	int n = 0;
 	int sum = 0;
-
 	printf("n = ");
-	
 	do{
 		scanf("%d" , &n);
 	}while(n < 0);
 
-	for(int i = 0 ; i < n ; i++){
-		for(int j = 0 ; j < n ; j++){
-			
-			if(i >= j){
-				sum++;
-				printf(" %d " , sum);
-			}
-			
-			else{ printf("   ");}
+	for(int i = 1 ; i <= n ; i++)
+	{
+		for(int j = 1 ; j <= i; j++)
+		{
+			sum++;
+			printf(" %d " , sum);
 		}
 		printf("\n");
 	}
-	
 return 0;
 }
